64381af198c8eab1ca47ac1eb813cd84bc6f3a5e
Andrew <49621025+andrei-voia@users.noreply.github.com>
2019-04-27 00:57:17 +0300

    pics and code update

---
+++ Launcher.java
@@ -0,0 +1,11 @@
+package code.main;
+
+public class Launcher{
+		
+	public static void main(String[] args)
+	{
+		//maybe use this for better performance ? have to test it on other weaker laptops
+		//System.setProperty("sun.java2d.opengl","True");
+		new GameBehavior();
+	}
+}
